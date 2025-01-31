989fe30bc221a07904fd8e1ac68842f31bc89dde
GDBILLION <godwindpius@gmail.com>
2025-01-31 10:31:25 +0100

    Stage 0 Backend API

---
+++ Controllers/HNGController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNG12API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HNGController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDetails()
+        {
+            var response = new
+            {
+                email = "godwindpius@gmail.com",
+                current_datetime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                github_url = "https://github.com/GDBILLION/HNG12_STEP_0_API.git"
+            };
+
+            return Ok(response);
+        }
+    }
+}
